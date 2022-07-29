520c57308ce61b1ecea39c13aec4a2df50ef9c23
Nio.Zhang <929354247@qq.com>
2022-07-29 17:35:21 +0800

    update

--- excel-to-array.es.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/index'
-import excelToArray from './lib/index'
-export default excelToArray
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { setupCounter } from "./counter"
-import excelToArray from "../lib/excel-to-array.es.js"
+import excelToArray from "../lib/excel2array-plus.es"
 
 console.log(
   excelToArray(`Wenwen	用车，交通 （2 辆大巴 ）从公司出发。 清点人数			
@@ -11,22 +10,3 @@ console.log(
 刘浩+Megan"	"运动-》 酒店 check in (换洗衣服）-> 烧烤 ( 颁奖-毛巾。 9新人介绍. 穿插小游戏）
 - 热菜，提前送过来 （结束后，统一回酒店）。9:30 之后，自由活动时间"			`)
 )
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
